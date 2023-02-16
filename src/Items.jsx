0d8c8151a314556f044b2ba032de895542ddeb8f
Keerthanachozan <113892405+Keerthanachozan@users.noreply.github.com>
2023-02-16 11:30:57 +0530

    Add files via upload

---
+++ src/Items.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../src/Items.css";
+
+const Items = ({ setShow, size }) => {
+  return (
+    <nav>
+      <div className="nav_box">
+        <span className="my_shop" onClick={() => setShow(true)}>FRUITS</span>
+        <div className="cart" onClick={() => setShow(false)}>
+          <span>
+            <i className="fas fa-cart-plus"></i>
+          </span>
+          <span>{size}</span>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Items;
